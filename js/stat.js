41f4c72beba1b2ba4765ddd3252280546c92746b
Kirill B <borunovx@gmail.com>
2018-04-08 20:30:31 +0300

    Коррекция №1
    В общем я вытащил то, что смог вытащить, объявил дополнительные переменные, чтобы код был не таким громоздким.
    Поправил цвета и текст и все такое.

--- js/stat.js
+++ js/stat.js
@@ -1,43 +1,51 @@
-var renderStatistics = function (ctx, names, times) {
-  var CLOUD_WIDTH = 420;
-  var CLOUD_HEIGHT = 270;
-  var BAR_HEIGHT = 150;
-  var BAR_WIDTH = 40;
-  var BAR_GAP = 50;
-  var CLOUD_X = 100;
-  var CLOUD_Y = 10;
-  var CLOUD_GAP = 10;
-  var barCorrection = CLOUD_HEIGHT / 9;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_GAP = 10;
+var BAR_X = CLOUD_X + BAR_WIDTH;
+var barCorrection = CLOUD_HEIGHT / 9;
+var gap = BAR_WIDTH + BAR_GAP;
+
+var getMaxTime = function (times) {
   var maxTime = times[0];
-  var canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
-
-  var renderCloud = function (context, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-  }; // функция отрисовки окна статистики
-
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgb(0, 0, 0)'); // рисуем тень
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgb(7, 245, 7)'); // рисуем белый прямоугольник
-
   for (var i = 1; i <= times.length - 1; i++) {
     if (maxTime < times[i]) {
       maxTime = times[i];
     }
-  } // ищем наибольшее время
+  }
+  return maxTime; // находим максимальное время
+};
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+}; // функция отрисовки окна статистики
+
+var renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)'); // рисуем тень
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)'); // рисуем белый прямоугольник
+  getMaxTime(times);
   ctx.font = '16px pt mono'; // задаем шрифт
   ctx.fillStyle = 'rgb(0, 0, 0)'; // задаем цвет
-  ctx.fillText('Ура вы победили!', CLOUD_X + 2 * CLOUD_GAP, CLOUD_Y + CLOUD_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + 2 * CLOUD_GAP, CLOUD_Y + 2 * CLOUD_GAP);
-  for (i = 0; i < names.length; i++) {
+  ctx.fillText('Ура вы победили!', CLOUD_X + 2 * CLOUD_GAP, CLOUD_Y + 3 * CLOUD_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + 2 * CLOUD_GAP, CLOUD_Y + 5 * CLOUD_GAP);
+  for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillText(names[i], CLOUD_X + BAR_WIDTH + (i * (BAR_WIDTH + BAR_GAP)), CLOUD_HEIGHT - CLOUD_GAP);
+    ctx.fillText(names[i], BAR_X + (i * gap), CLOUD_HEIGHT - CLOUD_GAP);
     if (names [i] === 'Вы') {
       ctx.fillStyle = 'rgb(227, 5, 30)';
     } else {
-      ctx.fillStyle = 'rgb(0, 8, 237)';
+      ctx.fillStyle = 'rgb(' + Math.round(155 - 42.5 * i) + ',' +
+        Math.round(155 - 42.5 * i) + ', 255)';
     }
-    var actualTime = Math.round((BAR_HEIGHT * times[i]) / maxTime);
-    ctx.fillRect(CLOUD_X + BAR_WIDTH + (i * (BAR_WIDTH + BAR_GAP)), CLOUD_HEIGHT - barCorrection - actualTime, BAR_WIDTH, actualTime);
+    var actualTime = Math.round((BAR_HEIGHT * times[i]) / getMaxTime(times));
+    var barY = CLOUD_HEIGHT - barCorrection - actualTime - CLOUD_GAP;
+    ctx.fillRect(BAR_X + (i * gap), barY + CLOUD_GAP, BAR_WIDTH, actualTime);
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillText(Math.round(times[i]), BAR_X + (i * gap), barY);
   } // рисуем статистику
 };
